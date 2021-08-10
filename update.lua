610fe992ed2fe120f780399e5041ef84c4645386
Ozomo <77997707+Ozomo@users.noreply.github.com>
2021-08-10 02:58:01 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,8 @@
 shell.run("cd ..")
 shell.run("github clone Ozomo/computercraft -b main")
-shell.run("delete update.lua")
-shell.run("move computercraft/update.lua /")
+if fs.exist(update.lua) do
+    shell.run("delete update.lua")
+    shell.run("move computercraft/update.lua /")
+else
+    shell.run("move computercraft/update.lua /")
+end
