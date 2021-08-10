b8b14dcba77dc64060fcd1dcdf357805552cbc8f
Ozomo <emil000320@gmail.com>
2021-08-10 14:02:24 +0200

    Update phone.lua

--- phone.lua
+++ phone.lua
@@ -4,6 +4,6 @@ while true do
 local event, modemSide, senderChannel, 
   replyChannel, message, senderDistance = os.pullEvent("modem_message")
     print(message.." ")
-    modem.transmit(1, 2, read())
+    modem.transmit(2, 1, read())
 end
 modem.close(2)
