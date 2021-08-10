182852e4529118268518511f078df9032f26c13e
Ozomo <emil000320@gmail.com>
2021-08-10 05:57:07 +0200

    Update receiver_modem.lua

--- receiver_modem.lua
+++ receiver_modem.lua
@@ -4,7 +4,8 @@ modem.open(2)
 while true do
 local event, modemSide, senderChannel, 
   replyChannel, message, senderDistance = os.pullEvent("modem_message")
-
-monit.write(message)
+monit.clear()
+monit.setCursorPos(1,1)
+monit.write(message.." ")
 end
 modem.close(2)
