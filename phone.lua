bb9bf6ac4eeaa896663bd1351b9af441dd78b3c6
Ozomo <emil000320@gmail.com>
2021-08-10 14:00:39 +0200

    .

--- phone.lua
+++ phone.lua
@@ -3,6 +3,7 @@ modem.open(2)
 while true do
 local event, modemSide, senderChannel, 
   replyChannel, message, senderDistance = os.pullEvent("modem_message")
-print(message.." ")
+    print(message.." ")
+    modem.transmit(1, 2, read())
 end
 modem.close(2)
